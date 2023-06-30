bcab6aeb5ed4bbb5a7379c7cdc6dd1f7aa3ad41f
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-06-29 21:54:08 -0400

    feat(web): add vercel analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
